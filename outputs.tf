13490e59244898defcceca76bef370ac15cb264c
Snehal Pansare <snehalpansare@Snehals-MacBook-Pro.local>
2022-02-14 18:11:56 +0530

    WKC on CPD
    Signed-off-by: Snehal Pansare <snehalpansare@Snehals-MacBook-Pro.local>

--- outputs.tf
+++ outputs.tf
@@ -38,29 +38,29 @@ output "cpd_namespace" {
 output "branch" {
   description = "The branch where the module config has been placed"
   value       = local.application_branch
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [null_resource.setup_instance_gitops]
 }
 
 output "namespace" {
   description = "The namespace where the module will be deployed"
   value       = local.namespace
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [null_resource.setup_instance_gitops]
 }
 
 output "server_name" {
   description = "The server where the module will be deployed"
   value       = var.server_name
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [null_resource.setup_instance_gitops]
 }
 
 output "layer" {
   description = "The layer where the module is deployed"
   value       = local.layer
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [null_resource.setup_instance_gitops]
 }
 
 output "type" {
   description = "The type of module where the module is deployed"
   value       = local.type
-  depends_on  = [null_resource.setup_gitops]
+  depends_on  = [null_resource.setup_instance_gitops]
 }
