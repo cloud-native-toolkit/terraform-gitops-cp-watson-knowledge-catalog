efe2c2a49924daa70ad214047e68a541c0253853
Snehal Pansare <snehalpansare@Snehals-MacBook-Pro.local>
2022-02-21 15:54:01 +0530

    Watson Knowldge Catalog on CP4D
    Signed-off-by: Snehal Pansare <snehalpansare@Snehals-MacBook-Pro.local>

--- main.tf
+++ main.tf
@@ -28,6 +28,7 @@ locals {
 
   layer = "services"
   operator_type  = "operators"
+  infra_type = "infrastructure"
   type = "instances"
   application_branch = "main"
   namespace = var.namespace
@@ -38,10 +39,51 @@ module setup_clis {
   source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
 }
 
+resource null_resource create_prerequisites_yaml {
+  provisioner "local-exec" {
+    command = "${path.module}/scripts/create-yaml.sh '${local.prerequisites_name}' '${local.prerequisites_yaml_dir}'"
+  }
+}
+
+resource null_resource setup_prerequisites_gitops {
+  depends_on = [null_resource.create_prerequisites_yaml]
+
+  triggers = {
+    name = local.prerequisites_name
+    namespace = var.namespace
+    yaml_dir = local.prerequisites_yaml_dir
+    server_name = var.server_name
+    layer = local.layer
+    type = local.infra_type
+    git_credentials = yamlencode(var.git_credentials)
+    gitops_config   = yamlencode(var.gitops_config)
+    bin_dir = local.bin_dir
+  }
+
+  provisioner "local-exec" {
+    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
+
+    environment = {
+      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+      GITOPS_CONFIG   = self.triggers.gitops_config
+    }
+  }
+
+  provisioner "local-exec" {
+    when = destroy
+    command = "${self.triggers.bin_dir}/igc gitops-module '${self.triggers.name}' -n '${self.triggers.namespace}' --delete --contentDir '${self.triggers.yaml_dir}' --serverName '${self.triggers.server_name}' -l '${self.triggers.layer}' --type '${self.triggers.type}'"
+
+    environment = {
+      GIT_CREDENTIALS = nonsensitive(self.triggers.git_credentials)
+      GITOPS_CONFIG   = self.triggers.gitops_config
+    }
+  }
+}
+
 module "gitops_sccs" {
   source = "github.com/cloud-native-toolkit/terraform-gitops-sccs.git?ref=v1.2.3"
 
-  #depends_on = [null_resource.setup_prerequisites_gitops]
+  depends_on = [null_resource.setup_prerequisites_gitops]
 
   gitops_config = var.gitops_config
   git_credentials = var.git_credentials
@@ -54,7 +96,7 @@ module "gitops_sccs" {
 module "gitops_rbac" {
   source = "github.com/cloud-native-toolkit/terraform-gitops-rbac.git?ref=v1.7.1"
 
-  #depends_on = [gitops_sccs]
+  depends_on = [null_resource.setup_prerequisites_gitops]
 
   gitops_config             = var.gitops_config
   git_credentials           = var.git_credentials
@@ -68,8 +110,7 @@ module "gitops_rbac" {
 }
 
 resource null_resource create_operator_yaml {
-
-  #depends_on = [gitops_rbac]
+  depends_on = [null_resource.setup_prerequisites_gitops]
 
   provisioner "local-exec" {
     command = "${path.module}/scripts/create-yaml.sh '${local.subscription_name}' '${local.subscription_yaml_dir}'"
@@ -116,6 +157,8 @@ resource null_resource setup_operator_gitops {
 }
 
 resource null_resource create_instance_yaml {
+  depends_on = [null_resource.setup_prerequisites_gitops]
+
   provisioner "local-exec" {
     command = "${path.module}/scripts/create-yaml.sh '${local.name}' '${local.instance_yaml_dir}' "
 
